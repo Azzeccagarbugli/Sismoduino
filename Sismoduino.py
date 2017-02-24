71ecc53d6d5b98058376dcc39e8274c60a9b0e06
Radeox <dawid.weglarz95@gmail.com>
2017-02-24 20:21:41 +0100

    Pulito codice, migliorata l'animazione del grafico e fixato il nome del file

--- Sismoduino.py
+++ Sismoduino.py
@@ -1,57 +1,59 @@
-# import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import serial
+# import serial
+# import numpy as np
 # import time
+import random
 
-Lim = 40
 
+# Configurazione grafico
 fig, ax = plt.subplots(facecolor='#333333')
 line, = ax.plot([], [], lw=2,  color='y')
 
+# Titolo
 plt.title('Grafico per il monitoraggio delle magnitudo', color='w')
 
-ax.set_facecolor('#333333')
-
-ax.set_ylim(-12000, 12000)
-plt.ylabel('Magnitudo', color='w')
-
+# Asse X
 ax.set_xlim(0, 60)
 plt.xlabel('Tempo', color='w')
 
+# Asse y
+ax.set_ylim(-12000, 12000)
+plt.ylabel('Magnitudo', color='w')
+
+ax.set_facecolor('#333333')
 ax.grid()
 
-xdata, ydata = [0]*100, [0]*100
-raw = serial.Serial("/dev/ttyACM0", 9600)
+xdata, ydata = [], []
 
-
-def update(data):
-    line.set_ydata(data)
-    return line,
+# Open serial USB
+# raw = serial.Serial("/dev/ttyACM0", 9600)
 
 
 def run(data):
     t, y = data
-    del xdata[0]
-    del ydata[0]
     xdata.append(t)
     ydata.append(y)
     line.set_data(xdata, ydata)
-    return line,
+
+    return line
 
 
 def data_gen():
     t = 0
+
     while True:
-        t += 0.1
-        ax.set_xbound(0, Lim)
+        t += 0.2
+        ax.set_xlim(t-30, t+30)
+
         try:
-            dat = int(raw.readline())
+            # val = int(raw.readline())
+            val = random.random() * 8000
         except:
-            dat = 0
-        yield t, dat
+            val = 0
+
+        yield t, val
 
 
 ani = animation.FuncAnimation(fig, run, data_gen, interval=0)
-# plt.pause(0.001)
 plt.show(block=True)
