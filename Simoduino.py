85a04bb87d6d37f9a4b49a0f24ce372a9fa2b275
Radeox <dawid.weglarz95@gmail.com>
2017-02-24 10:43:02 +0100

    Correzione codice seguendo lo standard

--- Simoduino.py
+++ Simoduino.py
@@ -1,41 +1,44 @@
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import serial
-import time
+# import time
 
 Lim = 40
 
-fig, ax = plt.subplots(facecolor = '#333333')
-line, = ax.plot([], [], lw=2,  color = 'y')
+fig, ax = plt.subplots(facecolor='#333333')
+line, = ax.plot([], [], lw=2,  color='y')
 
-plt.title('Grafico per il monitoraggio delle magnitudo', color = 'w')
+plt.title('Grafico per il monitoraggio delle magnitudo', color='w')
 
 ax.set_facecolor('#333333')
 
 ax.set_ylim(-12000, 12000)
-plt.ylabel('Magnitudo', color = 'w')
+plt.ylabel('Magnitudo', color='w')
 
 ax.set_xlim(0, 60)
-plt.xlabel('Tempo', color = 'w')
+plt.xlabel('Tempo', color='w')
 
 ax.grid()
 
 xdata, ydata = [0]*100, [0]*100
-raw = serial.Serial("/dev/ttyACM0",9600)
+raw = serial.Serial("/dev/ttyACM0", 9600)
+
 
 def update(data):
     line.set_ydata(data)
     return line,
 
+
 def run(data):
-    t,y = data
+    t, y = data
     del xdata[0]
     del ydata[0]
     xdata.append(t)
     ydata.append(y)
     line.set_data(xdata, ydata)
     return line,
+
 
 def data_gen():
     t = 0
@@ -48,6 +51,7 @@ def data_gen():
             dat = 0
         yield t, dat
 
+
 ani = animation.FuncAnimation(fig, run, data_gen, interval=0)
-#plt.pause(0.001)
+# plt.pause(0.001)
 plt.show(block=True)
